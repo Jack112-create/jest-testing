4e0169797ced4a90d9657568c792a6908f97b201
Jack112-create <jackconroy777@gmail.com>
2021-11-23 22:43:12 +0000

    Add choice ID tst

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -21,4 +21,13 @@
      test("currentGame key exists", () => {
          expect("currentGame" in game).toBe(true);
      });
+     test("playerMoves key exists", () => {
+         expect("playerMoves" in game).toBe(true);
+     });
+     test("choices key exists", () => {
+         expect("choices" in game).toBe(true);
+     });
+     test("choices contains correct ids", () => {
+         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+     });
  });
