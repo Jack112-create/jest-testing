bc3b52b4719e9f91c1c3e3a65f69aaf7597b3e94
Jack112-create <jackconroy777@gmail.com>
2021-11-24 23:09:02 +0000

    Add add turns function and test

--- scripts/game.js
+++ scripts/game.js
@@ -11,8 +11,14 @@ function newGame() {
     game.currentGame = []
 }
 
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))])
+    // showTurns();
+}
+
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore };
+module.exports = { game, newGame, showScore, addTurn };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,7 +3,7 @@
  * @jest-environment jsdom
  */
 
- const { game, newGame, showScore } = require("../game");
+ const { game, newGame, showScore, addTurn } = require("../game");
 
 
  beforeAll(() => {
@@ -39,7 +39,8 @@
         game.currentGame = ["button1", "button2"];
         document.getElementById("score").innerText = "42";
         newGame();
-        showScore()
+        showScore();
+        addTurn();
      });
      test("Should set game score to zero", () => {
         expect(game.score).toEqual(0);
@@ -47,9 +48,9 @@
      test("Should clear player moves", () => {
         expect(game.playerMoves.length).toBe(0);
      });
-     test("Should clear current game array", () => {
-        expect(game.currentGame.length).toBe(0);
-     });
+     test("Should be one move in the computer's array.", () => {
+        expect(game.currentGame.length).toBe(1);
+     })
      test("Should display 0 for the element with the ID of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
      });
