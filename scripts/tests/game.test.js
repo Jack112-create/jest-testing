3ee0729094900a5461bf6d709347ae9b0f36f6be
Jack112-create <jackconroy777@gmail.com>
2021-11-25 11:52:59 +0000

    Add test for incorrect move

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,11 @@
  * @jest-environment jsdom
  */
 
- const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
+ const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");
+
+ jest.spyOn(window, "alert").mockImplementation(() => {
+
+ })
 
  beforeAll(() => {
      let fs = require("fs");
@@ -82,6 +86,16 @@
          lightsOn(game.currentGame[0]);
          expect(button.classList).toContain(game.currentGame[0] + "light");
      });
+     test("should increment the score if the turn is correct", () => {
+         game.playerMoves.push(game.currentGame[0]);
+         playerTurn();
+         expect(game.score).toBe(1);
+     });
+     test("should call alert if the move is wrong", () => {
+         game.playerMoves.push("wrong");
+         playerTurn();
+         expect(window.alert).toBeCalledWith("Wrong move!")
+     })
      test("showTurns should update game.turnNumber", () => {
          game.turnNumber = 42;
          showTurns();
